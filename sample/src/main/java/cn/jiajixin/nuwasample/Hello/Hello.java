df6de5d91191dfe2f0c137c90f0272caeb9b9c3b
xiehonglin429 <alexclin0118@gmail.com>
2015-12-16 17:50:33 +0800

    增加构造函数

--- sample/src/main/java/cn/jiajixin/nuwasample/Hello/Hello.java
+++ sample/src/main/java/cn/jiajixin/nuwasample/Hello/Hello.java
@@ -4,7 +4,17 @@ package cn.jiajixin.nuwasample.Hello;
  * Created by jixin.jia on 15/11/5.
  */
 public class Hello {
+    private String word;
+
+    public Hello() {
+        word = "old word before fix";
+    }
+
     public String say() {
-        return "hello world";
+        return "say:"+word;
+    }
+
+    public static String testStatic(){
+        return "old static";
     }
 }
