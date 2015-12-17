626e907e35c0b733a6d4ee14b1d2ce018921047f
xiehonglin429 <alexclin0118@gmail.com>
2015-12-17 10:28:50 +0800

    完善测试

---
+++ sample/src/main/java/cn/jiajixin/nuwasample/Hello/TestModel.java
@@ -0,0 +1,28 @@
+package cn.jiajixin.nuwasample.Hello;
+
+/**
+ * com.euler.test
+ *
+ * @author:xiehonglin429
+ * @date 15/12/16 18:47
+ */
+public class TestModel {
+    public String filedStr = "newField";
+    public static String staticFiledStr = "newStaticFiled";
+    private String word;
+
+    public TestModel() {
+        word = "new after fix";
+    }
+
+    public String say() {
+        return "new say->" + word;
+    }
+
+
+    public static String testStatic() {
+        return "new static";
+    }
+}
+
+
--- sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
+++ sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
-import cn.jiajixin.nuwasample.Hello.Hello;
+import cn.jiajixin.nuwasample.Hello.TestModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        TextView textView = (TextView) findViewById(R.id.textview);
-        textView.setText(new Hello().say()+"\n"+Hello.testStatic());
+        TextView tv = (TextView) findViewById(R.id.textview);
+        TestModel model = new TestModel();
+        String str = String.format("TestSay:%s\nStaticMethod:%s\nField:%s\nStaticField:%s",
+                model.say(),TestModel.testStatic(),
+                model.filedStr,TestModel.staticFiledStr);
+        tv.setText(str);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        android.os.Process.killProcess(android.os.Process.myPid());
     }
 
 }
